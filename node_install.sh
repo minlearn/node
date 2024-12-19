7e77d34ed090fd9a4639a5a1cae3ebfb689cdf1c
minlearnminlearn <minlearn@minlearn.org>
2024-12-20 00:48:43 +0800

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -1,9 +1,9 @@
 ###############
 
+silent() { "$@" >/dev/null 2>&1; }
+
 echo "Installing Dependencies"
-apt-get install -y curl
-apt-get install -y sudo
-apt-get install -y mc
+silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
 wget --no-check-certificate https://nodejs.org/dist/v18.20.5/node-v18.20.5-linux-x64.tar.gz -O /tmp/node.tar.gz
@@ -11,8 +11,8 @@ tar xzvf /tmp/node.tar.gz --exclude CHANGELOG.md --exclude LICENSE --exclude REA
 rm -rf /tmp/node.tar.gz
 
 echo "Cleaning up"
-apt-get -y autoremove
-apt-get -y autoclean
+silent apt-get -y autoremove
+silent apt-get -y autoclean
 echo "Cleaned"
 
 ##############
